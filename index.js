fef36653361cf5986f53ed174ae2105593e4e06e
Wayne <dev.wayne.ix@gmail.com>
2015-06-16 20:18:51 +0800

    init

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 /**
  * @return {void} description
  */
-function Dom(){
+var Dom = {
+	parse:parse
 
 }
 
-typeof module != 'undefined' ? module.exports = Dom : this[Dom] = Dom;
+function parse(html, context) {
+    if (typeof html !== 'string') return;
+    context = context || document;
+    var m = /<([\w:]+)/.exec(html);
+    if (!m) return context.createTextNode(html);
+
+    // Remove leading/trailing whitespace
+    html = html.replace(/^\s+|\s+$/g, '');
+
+    var el = context.createElement('div');
+    el.innerHTML = html;
+
+    // one element
+    if (el.firstChild == el.lastChild) {
+        return el.removeChild(el.firstChild);
+    }
+
+    // several elements
+    var fragment = doc.createDocumentFragment();
+    while (el.firstChild) {
+        fragment.appendChild(el.removeChild(el.firstChild));
+    }
+
+    return fragment;
+}
+
+
+module.exports = {Dom:Dom};
